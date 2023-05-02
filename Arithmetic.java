d8a898a4ed6e91bab22c16880034c6b3fa792cbe
EMATIU <tiu_ema@yahoo.com>
2023-05-02 17:14:51 +0300

    ex1

---
+++ Arithmetic.java
@@ -0,0 +1,21 @@
+import java.math.BigDecimal;
+
+public class Arithmetic {
+
+    public Double add(Double a, Double b){
+        return BigDecimal.valueOf(a).add(BigDecimal.valueOf(b)).doubleValue();
+    }
+
+    public Double substract(Double a, Double b){
+        return BigDecimal.valueOf(a).subtract(BigDecimal.valueOf(b)).doubleValue();
+    }
+
+    public Double multiply(Double a, Double b){
+        return BigDecimal.valueOf(a).multiply(BigDecimal.valueOf(b)).doubleValue();
+    }
+
+    public Double divide(Double a, Double b){
+        return BigDecimal.valueOf(a).divide(BigDecimal.valueOf(b)).doubleValue();
+    }
+
+}
---
+++ ArithmeticTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArithmeticTest {
+
+    @DisplayName("Addition")
+    @CsvSource({"-1, 5, 4", "100, 12, 112", "4.5, 1.1, 5.6"})
+    void Add(double a, double b, double c) {
+        var arithmetic = new Arithmetic();
+        assertEquals(c, arithmetic.add(a, b));
+    }
+
+    @DisplayName("Substraction")
+    @CsvSource({"-10, -20, -30", "100, 12, 88", "4.5, 1.1, 3.4"})
+    void Subst(double a, double b, double c) {
+        var arithmetic = new Arithmetic();
+        assertEquals(c, arithmetic.substract(a, b));
+    }
+
+    @DisplayName("Multiply")
+    @CsvSource({"-1, 5, -5", "100, 12, 1200", "2, 1.1, 2.2"})
+    void Multiply(double a, double b, double c) {
+        var arithmetic = new Arithmetic();
+        assertEquals(c, arithmetic.multiply(a, b));
+    }
+
+    @DisplayName("Division")
+    @CsvSource({"-10, 5, -2", "100, 2, 50", "4.5, 2, 2.25"})
+    void Div(double a, double b, double c) {
+        var arithmetic = new Arithmetic();
+        assertEquals(c, arithmetic.divide(a, b));
+    }
+}
